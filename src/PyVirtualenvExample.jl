95b2744a0df4179949f5138e53ac587e92a0afe4
Takafumi Arakaki <aka.tkf@gmail.com>
2018-09-17 23:42:56 -0700

    Demonstrate activate_pipenv

--- src/PyVirtualenvExample.jl
+++ src/PyVirtualenvExample.jl
@@ -1,5 +1,15 @@
 module PyVirtualenvExample
 
-# package code goes here
+using PyVirtualenv
+
+function __init__()
+    PyVirtualenv.activate_pipenv(@__MODULE__)
+    @eval using PyCall
+end
+
+function demo()
+    sys_executable = pyimport("sys")[:executable]
+    @info "Using: sys.executable = $sys_executable"
+end
 
 end # module
